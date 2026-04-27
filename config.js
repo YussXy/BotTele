eb97d0a995de475141eccaab2277a1946aad46a3
YussXy <yussalmount@gmail.com>
2026-04-27 13:45:29 +0700

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-
-//Edit
+//Token pakai token bot tele lu
 
 module.exports = {
-  token: '8774824227:AAGgEJjk9DlwnYEDXIYwqEyPsMuh4sDe2_0',
+  token: '',
   namaBot: 'SyuzoBot V1',
   owner: 'Yuss Xy',
   noWa: '6283183469343'
